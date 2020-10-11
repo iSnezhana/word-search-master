66bbb42e5dc1e979fa563dbec0ac94e4c7a00602
Pravin Bhat <bhatpravin@gmail.com>
2020-10-11 09:04:37 -0400

    Initial commit - word-search app generated from start.spring.io

---
+++ src/main/java/com/bhatman/proto/wordsearch/WordSearchApplication.java
@@ -0,0 +1,13 @@
+package com.bhatman.proto.wordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSearchApplication.class, args);
+	}
+
+}
